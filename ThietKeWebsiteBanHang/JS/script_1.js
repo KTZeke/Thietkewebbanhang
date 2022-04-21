c1cda3b770aa26da7c83bc0300282a9f76d222e9
KTZeke <104152261+KTZeke@users.noreply.github.com>
2022-04-21 21:45:57 +0700

    Add files via upload

---
+++ ThietKeWebsiteBanHang/JS/script_1.js
@@ -0,0 +1,32 @@
+function announcement(){
+	alert("Website phục vụ cho học phần thiết kế web, không được tạo ra dựa trên mục đích thương mại! \nBản quyền thuộc về công ty YODY VN \nCopyright by YODY VN Company");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("hinhslide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
